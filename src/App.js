0ddb0a647baab41a073045a46e26c1ca165c98bf
chaijewon <vcandjava@nate.com>
2020-03-28 15:20:54 +0900

    hook4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React,{useReducer} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+// action => event
+function reducer(state,action){
+  switch(action.type)
+  {
+    case 'INCREMENT':
+      return {value:state.value+1}
+    case 'DECREMENT':
+      return {value:state.value-1}
+    default:
+      return state;
+  }
+}
+/*
+      dispatch({type:'INCREMENT'})
+               ================== action.type (함수)
+      reducer(state,action)
+      {
+           case 'INCREMENT':
+            return {value:state.value+1}
+          case 'DECREMENT':
+            return {value:state.value-1}
+          default:
+            return state;
+      }
+ */
 function App() {
+  const [state,dispatch]=useReducer(reducer,{value:0})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>현재 state의 값:{state.value}</p>
+      <button onClick={()=>dispatch({type:'INCREMENT'})}>+</button>
+      <button onClick={()=>dispatch({type:'DECREMENT'})}>-</button>
     </div>
   );
 }
